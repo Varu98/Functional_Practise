f070b619ec2c46986abeaa7b5ea25717e082a5bf
Varu <vardaan.agarwal.98@gmail.com>
2022-03-02 12:37:35 +0530

    add: questions

---
+++ functionalProgrammingExcercises/filterTanayCycle.js
@@ -0,0 +1,26 @@
+const family = [
+  {
+    name: "Tanay",
+    haveCycle: true,
+  },
+  {
+    name: "Akanksha",
+    haveCycle: false,
+  },
+  {
+    name: "Tanvi",
+    haveCycle: true,
+  },
+  {
+    name: "Kanak",
+    haveCycle: false,
+  },
+];
+
+//   Your output should be: ['Tanay', 'Tanvi']
+
+const findPersonWithCycle = (arr) => arr.haveCycle === true;
+
+const output = family.filter(findPersonWithCycle);
+
+output.map((e) => e.name);
